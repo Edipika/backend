25bcbd002c5a7a3c2b92f2ec134c6dba014af8cd
Backend-dipika <Jrbackend@samsdigital.in>
2025-01-29 16:46:18 +0530

    changes

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -2,15 +2,15 @@ const express = require('express');
 const router = express.Router();
 // const { adminlogin } = require('../controllers/login-controller');
 const { showCategories } = require('../controllers/categoryController');
-const verifyToken = require('../middleware/jwtMiddleware');
+
 
 
 // Define the POST route for adding a category
 // router.post('/adminLogin', adminlogin);
-router.get('/protected', verifyToken, showCategories);
+router.get('/protected' , showCategories);
 
-router.post('/verify-token', verifyToken,showCategories);
-router.get('/verify-token', verifyToken);
+router.post('/verify-token',showCategories);
+router.get('/verify-token', );
 // router.get('/adminLogin',adminlogin);
 
 module.exports = router; // Export the router
